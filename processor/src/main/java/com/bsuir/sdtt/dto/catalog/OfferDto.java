86a3708b5b51a1387fd1a1d43730ca2ba3a2e7f2
steppbol <boloshenko.s@gmail.com>
2019-03-27 21:47:02 +0300

    feat: add

---
+++ processor/src/main/java/com/bsuir/sdtt/dto/catalog/OfferDto.java
@@ -0,0 +1,58 @@
+package com.bsuir.sdtt.dto.catalog;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Class of Offer Data Transfer Object. Used to transfer data between application subsystems.
+ *
+ * @author Stsiapan Balashenka
+ * @version 1.0
+ */
+@Data
+@Builder
+@AllArgsConstructor
+public class OfferDto {
+    private UUID id;
+
+    /**
+     * Field of seller ID.
+     */
+    @NotNull
+    private UUID sellerId;
+
+    /**
+     * Field of Offer name.
+     */
+    @NotNull
+    private String name;
+
+    /**
+     * Field of Offer category.
+     */
+    @NotNull
+    private String category;
+
+    @NotNull
+    private String description;
+
+    /**
+     * Field of Offer price.
+     */
+    @NotNull
+    @Min(0)
+    private double price;
+
+    private List<CommentDto> comments = new ArrayList<>();
+
+    public OfferDto() {
+
+    }
+}
