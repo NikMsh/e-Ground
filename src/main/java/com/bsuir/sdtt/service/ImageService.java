2a0e41fdf20f43f2cffe5fc4caa3b179aba5572e
steppbol <boloshenko.s@gmail.com>
2019-05-03 17:33:43 +0300

    feat: add module

---
+++ src/main/java/com/bsuir/sdtt/service/ImageService.java
@@ -0,0 +1,21 @@
+package com.bsuir.sdtt.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+/**
+ * Interface of google api image service.
+ *
+ * @author Stsiapan Balashenka
+ * @version 1.0
+ */
+public interface ImageService {
+    File convertStringToFile(String image) throws IOException;
+
+    String saveImageToGoogleDrive(java.io.File image) throws GeneralSecurityException, IOException;
+
+    String compressionImage(java.io.File image) throws IOException, GeneralSecurityException;
+
+    void deleteImageFromGoogleDrive(String imageId, String compressImageId) throws GeneralSecurityException, IOException;
+}
