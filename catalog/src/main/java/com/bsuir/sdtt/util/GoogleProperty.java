4a25addae5d90c2c95fef6b3f09bfdc2e283e7cd
steppbol <boloshenko.s@gmail.com>
2019-05-03 18:06:52 +0300

    feat:

--- catalog/src/main/java/com/bsuir/sdtt/util/GoogleProperty.java
+++
@@ -1,9 +0,0 @@
-package com.bsuir.sdtt.util;
-
-public abstract class GoogleProperty {
-    public static final String APPLICATION_NAME = "e-Ground";
-
-    public static final String TOKENS_DIRECTORY_PATH = "tokens";
-
-    public static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-}
